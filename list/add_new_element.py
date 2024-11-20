a96b24c20ba2c661927b8d8ad63eaddc9c3f2dda
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-20 19:19:09 +0530

    Update add_new_element.py

--- list/add_new_element.py
+++ list/add_new_element.py
@@ -1,7 +1,7 @@
 list2=list(map(int , input("Enter The List: ").split()))  # 1 2 3 4 
 start_index=len(list2)-1# 0->1  1->2 
 end_index=0# 3-> 4  , 
-new_list=[5] # empty 
+new_list=list(map(int, input("Entr the List  new Values: ").split())) # empty 
 while start_index>=end_index:  # 3 >=0
      new_list.append(list2[start_index]) #1 
      start_index-=1
